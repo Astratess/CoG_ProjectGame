ccf660d5e809747fad2596a28f6f46441e16af12
Astrates <st.kostow@gmail.com>
2022-07-26 01:33:20 +0300

    Fix Img

--- js/index.js
+++ js/index.js
@@ -59,15 +59,15 @@ solaSelect.addEventListener('click',()=>{
     localStorage.setItem('currentPlayer','Sola')
     localStorage.setItem('Sola', localStorage.getItem('Sola'))
     localStorage.setItem('character', localStorage.getItem('Sola'))
-    localStorage.setItem('body', "img/sola/bodysola.png")
+    localStorage.setItem('body',`img/sola/bodysola.png`)
     localStorage.setItem('inventory', localStorage.getItem("inventorySola"))
-    document.getElementById('mimi').style.background = "ulr('img/sola/sola.bg.jpg')"
+    document.getElementById('mimi').style.background = "url('img/sola/sola.bg.jpg')"
 })
 
 brannSelect.addEventListener('click',()=>{
     localStorage.setItem('currentPlayer','Brann')
     localStorage.setItem('Brann', localStorage.getItem('Brann'))
     localStorage.setItem('character', localStorage.getItem('Brann'))
-    localStorage.setItem('body', "img/brann/body.png")
+    localStorage.setItem('body',`img/brann/body.png`)
     localStorage.setItem('inventory', localStorage.getItem("inventoryBrann") )
 })
